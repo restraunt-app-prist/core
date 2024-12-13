ff382998a5d9a70990024a2574fe8cc1e85d6851
Maksym <rendon4ik@gmail.com>
2024-12-13 17:28:14 +0200

    Fix minor issues with menu item entity

--- src/main/java/kpi/fict/prist/core/menu/entity/MenuItemEntity.java
+++ src/main/java/kpi/fict/prist/core/menu/entity/MenuItemEntity.java
@@ -1,12 +1,11 @@
 package kpi.fict.prist.core.menu.entity;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.annotation.PersistenceCreator;
+import org.springframework.data.domain.Persistable;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import kpi.fict.prist.core.common.MenuCategory;
@@ -17,9 +16,9 @@ import java.time.LocalDateTime;
 @Data
 @Builder
 @NoArgsConstructor
-@AllArgsConstructor
+@AllArgsConstructor(onConstructor_ = @PersistenceCreator)
 @Document("menu_items")
-public class MenuItemEntity {
+public class MenuItemEntity implements Persistable<String> {
 
     @Id
     private String id;
@@ -35,4 +34,9 @@ public class MenuItemEntity {
 
     @LastModifiedDate
     private LocalDateTime updatedAt;
+
+    @Override
+    public boolean isNew() {
+        return id == null;
+    }
 }
