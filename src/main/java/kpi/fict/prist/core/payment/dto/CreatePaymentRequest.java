5dacee1b8cc83ce7934f1c393e797d4d85f07475
Bogdan Zaranik <bogdan.zaranik@appliedmethods.biz>
2025-01-02 17:20:53 +0200

    payment with orders integration

--- src/main/java/kpi/fict/prist/core/payment/dto/CreatePaymentRequest.java
+++
@@ -1,9 +0,0 @@
-package kpi.fict.prist.core.payment.dto;
-
-import jakarta.validation.constraints.NotNull;
-
-public record CreatePaymentRequest(
-    @NotNull Long amount,
-    @NotNull String description
-) {
-}
